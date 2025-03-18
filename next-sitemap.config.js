47698c45878b9b2fa277e7b5d30659a87b841da7
abhishek-yadav18 <abhishek.yadav@ascendion.com>
2025-03-18 15:54:51 +0530

    added sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: "https://notes-app-abhi.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "daily",
+  priority: 0.7,
+};
--- src/app/next-sitemap.js
+++
@@ -1,6 +0,0 @@
-// next-sitemap.js
-export const siteUrl = "https://notes-app-abhi.vercel.app";
-export const generateRobotsTxt = true;
-export const sitemapSize = 7000;
-export const changefreq = "daily";
-export const priority = 0.7;
