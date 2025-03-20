1b07685aacba0041bb14e58b3d949c8fc9dca32e
abhishek-yadav18 <abhishek.yadav@ascendion.com>
2025-03-20 12:00:03 +0530

    added ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ import { AuthProvider, useAuth } from "@/hooks/use-auth";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/hooks/theme";
 import DynamicTitle from "@/components/DynamicTitle";
+import Script from "next/script";
 
 const AppContent = ({ children }) => {
   const { user } = useAuth();
@@ -37,10 +38,10 @@ export default function RootLayout({ children }) {
               name="google-site-verification"
               content="zyFST-yppUMiy2xHAPNodmjmuafTgghQ35Z3IHrh2oo"
             />
-            <script
+            <Script
               type="text/javascript"
               src="//pl26165613.effectiveratecpm.com/47/68/e7/4768e74be5885fafaf08d82a6171d7a5.js"
-            ></script>
+            ></Script>
           </head>
           <body>
             <AppContent>{children}</AppContent>
